a5479f8d49e50be22052fb0ef005d754bdb80b45
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-14 16:20:28 +0100

    Define Customer class skeleton

--- src/python/movierental.py
+++ src/python/movierental.py
@@ -8,15 +8,39 @@ This program model a movie rental office (shope). The main components are:
 - RentalShop: a rental shop class
 """
 from typing import List
+from enum import Enum, auto
+
+
+class RentStatus(Enum):
+    CHECKIN = auto()
+    CHECKOUT = auto()
+
 
 
 class Movie:
     """Movie class."""
-    __slots__ = ("_title", "_stars", "_year", "_prodco", "_director")
+    __slots__ = ("_title", "_stars", "_year", "_prodco", "_director", "_status",
+        "_count")
 
     def __init__(self, title :str=None, stars: List[str]=None,
         year:int =None, prodco: str=None, director: List=None):
         """"""
+        pass
+
+    @property
+    def status(self) -> bool:
+        pass
+
+    @status.setter
+    def status(self, flag:bool):
+        pass
+
+    @property
+    def count(self) -> int:
+        pass
+
+    @count.setter
+    def count(self, flag: RentStatus):
         pass
 
     @property
@@ -60,3 +84,52 @@ class Movie:
     def __repr__(self) -> str:
         pass
 
+
+class Customer:
+    """Customer class."""
+
+    __slots__ = ("_fname", "_lname", "_email", "_movies", "_cid")
+
+    def __init__(self, fname: str=None, lname: str=None, email: str=None):
+        pass
+
+    @property
+    def firstname(self) -> str:
+        pass
+
+    def set_firstname(self, fname: str) -> None:
+        pass
+
+    @property
+    def lastname(self) -> str:
+        pass
+
+    def set_lastname(self, lname: str) -> None:
+        pass
+
+    @property
+    def email(self) -> str:
+        pass
+
+    def set_email(self, email:str) -> None:
+        pass
+
+    @property
+    def customer_id(self) -> str:
+        pass
+
+    @property
+    def movies(self) -> List[Movie]:
+        pass
+
+    def addmovie(self, movie: Movie) -> None:
+        pass
+
+    def retmovie(self, movie: Movie) -> None:
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
