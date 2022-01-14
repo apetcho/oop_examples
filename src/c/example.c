3e77c598f22ae596ffa9b5e11b6aa69ca0ba5423
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-14 11:53:04 +0100

    Define Shape vtable

--- src/c/example.c
+++ src/c/example.c
@@ -4,5 +4,15 @@
 
 /* Shape abstract interface */
 struct Shape {
-    struct ShapeFunctTable *funTable;
+    struct ShapeFunTable *funTable;
 };
+
+struct ShapeFunTable {
+    void (*draw)(struct Shape* object);
+    void (*moveto)(struct Shape* object, int x, int y);
+    void (*destroy)(struct Shape *object);
+};
+
+// never create an instance shape directly: this make it an abstract interface
+struct Shape* Shape_new(){ assert(0); }
+
