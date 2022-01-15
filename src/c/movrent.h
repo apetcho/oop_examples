67ec4b553b1d275b9ac591a63dd6c62b32c9449f
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-15 02:21:41 +0100

    Define Customer interface

--- src/c/movrent.h
+++ src/c/movrent.h
@@ -24,7 +24,7 @@ void* Vector_get_item(Vector*, size_t);
 void Vector_set_item(Vector*, size_t, void*);
 size_t Vector_get_size(Vector*);
 size_t Vector_get_capacity(Vector*);
-void Vector_set_capacity(Vector*);
+void Vector_set_capacity(Vector*, size_t);
 char* Vector_to_string(Vector*);
 void Vector_print(const Vector*);
 size_t Vector_fget(FILE*, Vector*);
@@ -42,9 +42,9 @@ size_t Person_fput(FILE*, const Person*);
 char* Person_get_first_name(const Person*);
 char* Person_get_last_name(const Person*);
 char* Person_get_email(const Person*);
-void Person_set_first_name(Person*);
-void Person_set_last_name(Person*);
-void Person_set_email(Person*);
+void Person_set_first_name(Person*, const char*);
+void Person_set_last_name(Person*, const char*);
+void Person_set_email(Person*, const char*);
 
 // Movie class
 Movie* Movie_create();
@@ -54,17 +54,27 @@ void Movie_print(const Movie*);
 size_t Movie_fget(FILE*, Movie*);
 size_t Movie_fput(FILE*, const Movie*);
 char* Movie_get_title(const Movie*);
-char* Movie_get_year(const Movie*);
+int Movie_get_year(const Movie*);
 char* Movie_get_production(const Movie*);
 Person* Movie_get_director(const Movie*);
-Vector Movie_get_stars(const Movie*);
-void Movie_set_title(Movie*);
-void Movie_set_year(Movie*);
-void Movie_set_production(Movie*);
-void Movie_set_director(Movie*);
-void Movie_set_stars(Movie*);
+Vector* Movie_get_stars(const Movie*);
+void Movie_set_title(Movie*, const char*);
+void Movie_set_year(Movie*, int);
+void Movie_set_production(Movie*, const char*);
+void Movie_set_director(Movie*, const Person*);
+void Movie_set_stars(Movie*, const Vector*);
 
 // Customer class
+Customer* Customer_create();
+void Customer_destroy(Customer*);
+char* Customer_to_string(Customer*);
+void Customer_print(const Customer*);
+size_t Customer_fget(FILE*, Customer*);
+size_t Customer_fput(FILE*, const Customer*);
+Person* Customer_get_info(const Customer*);
+Vector* Customer_get_movies(const Customer*);
+void Customer_set_info(Customer*, Person*);
+void Customer_set_movies(Customer*, Vector*);
 
 // MRObject abstract class
 
